bce9202c461554d59c47ed075dcd9bea31eb3111
=Valere <=Valere.AtanganaNoah@videotron.com>
2020-04-21 10:09:35 -0400

    Rabbit mq demo

---
+++ src/main/java/com/rabbitlistenerproject/rabbitlistenerprojectdemo/RabbitlistenerprojectdemoApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitlistenerproject.rabbitlistenerprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitlistenerprojectdemoApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitlistenerprojectdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitlistenerproject/rabbitlistenerprojectdemo/RabbitlistenerprojectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitlistenerproject.rabbitlistenerprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitlistenerprojectdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
